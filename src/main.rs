8832d4af38d0d5b6882cab4e1bde035035452ccb
Grady Flanary <grady.flanary@gmail.com>
2022-07-09 17:14:13 -0500

    Initial commit for a Rust telnet chat app.

---
+++ src/main.rs
@@ -0,0 +1,51 @@
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt, BufReader, AsyncBufReadExt}, 
+    net::TcpListener, sync::broadcast};
+
+
+#[tokio::main]
+async fn main() {
+    let listener = TcpListener::bind("localhost:8080").await.unwrap();
+    
+    let (tx, mut _rx) = broadcast::channel::<String>(10);
+    
+    
+    loop{
+        let (mut socket, _addr) = listener.accept().await.unwrap();
+        
+        let tx = tx.clone();
+        let mut rx = tx.subscribe();
+
+        tokio::spawn(async move{
+            let (read, mut writer) = socket.split();
+            
+            let mut reader = BufReader::new(read);
+
+
+
+            let mut line = String::new();
+
+            loop{
+
+                tokio::select! {
+                    result = reader.read_line(&mut line) => {
+                        if result.unwrap() == 0{
+                            break;
+                        }
+                        tx.send(line.clone()).unwrap();
+                        line.clear();
+                    } 
+
+                    result = rx.recv() => {
+                        let msg = result.unwrap();
+                        writer.write_all(&msg.as_bytes()).await.unwrap();
+                    }
+
+                }
+
+
+            }
+        });
+    }
+}
+
